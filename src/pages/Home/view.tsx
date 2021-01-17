b4305870d5cdc1dafc8ca2314eae538dd0777291
unknown <vtelyachy@gmail.com>
2021-01-17 15:37:03 +0200

    added link

--- src/pages/Home/view.tsx
+++ src/pages/Home/view.tsx
@@ -3,10 +3,11 @@ import React from 'react'
 
 interface Props {
     isLoading?: boolean
+    goToTasks?: ()=>void
 }
 
-const View: React.FC<Props> = ({ isLoading }) => (<Layout showLoader={isLoading}>
-    Home
+const View: React.FC<Props> = ({ isLoading, goToTasks }) => (<Layout showLoader={isLoading}>
+    <p onClick={goToTasks}>Home</p>
     </Layout>)
 
 export default View
